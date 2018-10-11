fb99acdef4999550e882f25e9ed197bcea5669bb
‘Tim <timothy.j.ocampo@gmail.com>
2018-10-10 18:33:14 -0700

    added redux chrome extesion tools in store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, createStore} from "redux";
+import {applyMiddleware, createStore, compose} from "redux";
 import thunk from 'redux-thunk';
 import rootReducer from './reducers'
 const middleware =[thunk];
@@ -6,7 +6,9 @@ const initialState ={};
 
 // this is the basic layout for store for a reference:   ==> const store= createStore( ()=>[], {}, applyMiddleware);
 
-const store = createStore(rootReducer , initialState , applyMiddleware(...middleware));
+const store = createStore(rootReducer , initialState ,
+    compose(applyMiddleware(...middleware),  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+);
 
 export default store;
 //import to App.js to give to the provider
